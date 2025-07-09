9b0c53996d9d119048daf4b745de5285fe592584
AdamouGarba-web <adamougarbar@gmail.com>
2025-07-09 10:22:15 +0100

    Add files via upload

---
+++ hello_world_c.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]) // Début de la fonction
+{
+      printf ("Hello, World!\n"); // affiche à l'ecrant le message hello, world
+    return 0; // fin du programme
+}
