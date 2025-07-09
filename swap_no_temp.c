450a868911c12e06f9648913ad793da230729ac1
AdamouGarba-web <adamougarbar@gmail.com>
2025-07-09 10:49:26 +0100

    Add files via upload

---
+++ swap_no_temp.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main (int argc, char* argv[]){ // Début de la fonction
+int X, Y; // Déclaration des variables entières X et Y
+printf("Entrer la valeur de X :"); // Demande à l'utilisateur de saisir la valeur de X
+scanf("%d", &X); // lit la valeur saisie par l'utilisateur et l'assigne à X
+printf("Entrer la valeur de Y :"); // Demande à l'utilisateur de saisir la valeur de Y
+scanf("%d", &Y); // lit la valeur saisie par l'utilisateur et l'assigne à Y
+
+X = X+Y;
+Y = X-Y;
+X = X-Y;
+
+printf("Apres echange : X = %d, Y = %d\n", X, Y); // affiche les valeurs de X et Y aprés l'échange
+
+return 0; // Fin du programme
+}
