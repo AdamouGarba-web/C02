86107d2240d8b912f2c03ca20f81e5737e1c0b5d
AdamouGarba-web <adamougarbar@gmail.com>
2025-07-10 17:35:23 +0100

    Update swap_no_temp.c

--- swap_no_temp.c
+++ swap_no_temp.c
@@ -3,13 +3,13 @@
 int main () // Début de la fonction
 {
     int a, b; // Déclaration des variables entières a et b
-    scanf("%d %d", &a, &b); // lit la valeur saisie par l'utilisateur
+    scanf("%d %d", &a, &b); // lit la valeur saisie par l'utilisateur et l'assigne à a
 
     a = a + b;
     b = a - b;
     a = a - b;
 
-    printf("Après échange: a = %d, b = %d\n", a, b); // affiche les valeurs de a et b aprés l'échange
+    printf("Après échange : a = %d, b = %d\n", a, b); // affiche les valeurs de a et b aprés l'échange
 
     return 0; // Fin du programme
 }
