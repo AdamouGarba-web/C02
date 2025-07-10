5f63647145da6e6fa5bb50ca90ebe9a7fbd8536f
AdamouGarba-web <adamougarbar@gmail.com>
2025-07-10 12:13:26 +0100

    Update even_or_odd.c

--- even_or_odd.c
+++ even_or_odd.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main () { // Début de la fonction
     int nombre; // Déclaration de la variable nombre
-    printf("Entrez un entier : ");
+    printf("Entrez un entier : "); // Demande à l'utilisateur de sairir un entier
     scanf("%d", &nombre); // Lecture de l'entré saisi par l'utilisateur et le stocke dans la variablr nombre
 
     if (nombre % 2 == 0) { // si le reste de la division est égal à 0 si la condition est vraie
