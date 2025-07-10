e3fab08350c9ee4f0963147ba02afb20f5ecc0db
AdamouGarba-web <adamougarbar@gmail.com>
2025-07-10 17:41:56 +0100

    Update factorial.c

--- factorial.c
+++ factorial.c
@@ -4,13 +4,13 @@ int main() // Début de la fonction
 {
     int n, i; // Déclaration des variables
     long fact = 1;
-    scanf("%d", &n); // Lecture de l'entier saisi par l'utilisateur et le stocke dans N
+    scanf("%d", &n); // Lecture de l'entier saisi par l'utilisateur et le stocke dans n
 
     if (n < 0) // Veriffier si le nombre est negatif 
     {
         printf("La factorielle n'est pas définie pour les nombres negatifs.\n");
     } else
-        for (i = 1; i <= n; i++) // Boucle de 1 à N inclut pour la factorielle 
+        for (i = 1; i <= n; i++) // Boucle de 1 à n inclut pour la factorielle 
         {
             fact *= i; // Multiplie factoriel par i à chaque tour
         }
