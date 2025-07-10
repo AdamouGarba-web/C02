a91ca2fd49c0337a017d36f9c5a43b9d2e9911c9
AdamouGarba-web <adamougarbar@gmail.com>
2025-07-10 17:03:11 +0100

    Update swap_no_temp.c

--- swap_no_temp.c
+++ swap_no_temp.c
@@ -3,18 +3,13 @@
 int main () // Début de la fonction
 {
     int a, b; // Déclaration des variables entières a et b
-    printf("Entrez deux entiers : "); // Demande à l'utilisateur de saisir deux entiers
-    scanf("%d %d", &a, &b); // lit les deux valeur saisie par l'utilisateur
+    scanf("%d %d", &a, &b); // lit la valeur saisie par l'utilisateur
 
-    // Affichage avant échange
-    printf("Avant échange: a = %d, b = %d\n", a, b);
-
-    // Echange sans variable temporaire
     a = a + b;
     b = a - b;
     a = a - b;
 
-    printf("Apres echange : a = %d, b = %d\n", a, b); // affiche les valeurs de a et b aprés l'échange
+    printf("Après échange: a = %d, b = %d\n", a, b); // affiche les valeurs de a et b aprés l'échange
 
     return 0; // Fin du programme
 }
